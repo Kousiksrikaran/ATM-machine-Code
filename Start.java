d5ed9ea91deb4b68571a340dfa95396122e4de2f
Kousiksrikaran C <115222289+Kousiksrikaran@users.noreply.github.com>
2022-11-17 21:56:09 +0530

    Create Start.java

---
+++ Start.java
@@ -0,0 +1,20 @@
+package Atmmachine;
+
+import java.io.IOException;
+
+public class Start {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		
+		Start s=new Start();
+		Login l=new Login();
+		
+		
+		
+		l.getLogin();
+
+	
+	}
+
+}
